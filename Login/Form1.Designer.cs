d96071dcfeea507a123c0bed538b9be14c6b3639
陈秀 <Zhangchen.1008@qq.com>
2017-04-12 10:52:24 +0800

    完成用户添加和登录界面

--- Login/Form1.Designer.cs
+++ Login/Form1.Designer.cs
@@ -28,20 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(134, 112);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(100, 21);
+            this.txtKey.TabIndex = 9;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(134, 61);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(100, 21);
+            this.txtUserName.TabIndex = 8;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(109, 176);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 7;
+            this.btnEnter.Text = "确定";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "请输入密码：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "请输入用户名：";
             // 
             // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "FrmLogin";
             this.Text = "用户登录";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Login/Form1.cs
+++ Login/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Login
 {
@@ -15,6 +16,38 @@ namespace Login
         public FrmLogin()
         {
             InitializeComponent();
+        }
+        private DBHelper helper = new DBHelper();
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            string userName = this.txtUserName.Text.Trim();
+            string key = this.txtKey.Text.Trim();
+            int i = 0;
+            string password = "";
+            string strSQL = @"select LoginPwd from Admin where LoginId=@LoginId";
+            using (IDataReader reader = helper.ExecuteReader(strSQL, CommandType.StoredProcedure, new SqlParameter("@LoginId", userName)))
+            {
+                if (reader.Read())
+                {
+                    password = reader.GetString(reader.GetOrdinal("LoginPwd"));
+                    i++;
+                }
+            }
+            if (i == 0)
+            {
+                MessageBox.Show("用户名不存在！");
+            }
+            else
+            {
+                if (key == password)
+                {
+                    MessageBox.Show("登录成功");
+                }
+                else
+                {
+                    MessageBox.Show("您的密码不正确！");
+                }
+            }
         }
     }
 }
