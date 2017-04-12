45d5c9cc226029bb5d55e84acc7779288b9aa143
Administrator <Administrator@DEEP-20161028VQ>
2017-04-12 11:27:51 +0800

    Revert "修改了管理员的修改界面和删除界面"
    This reverts commit dcc74f39e7522431872420ecfea5a6d7eed473ac.

--- Login/FrmDeleteUser.Designer.cs
+++ Login/FrmDeleteUser.Designer.cs
@@ -28,154 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblPhone = new System.Windows.Forms.Label();
-            this.lblSex = new System.Windows.Forms.Label();
-            this.lblName = new System.Windows.Forms.Label();
-            this.lblLoginId = new System.Windows.Forms.Label();
-            this.lblCardID = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.lbl1 = new System.Windows.Forms.Label();
-            this.lblUser = new System.Windows.Forms.Label();
-            this.btnCertain = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // lblPhone
-            // 
-            this.lblPhone.AutoSize = true;
-            this.lblPhone.Location = new System.Drawing.Point(247, 338);
-            this.lblPhone.Name = "lblPhone";
-            this.lblPhone.Size = new System.Drawing.Size(17, 12);
-            this.lblPhone.TabIndex = 110;
-            this.lblPhone.Text = "11";
-            // 
-            // lblSex
-            // 
-            this.lblSex.AutoSize = true;
-            this.lblSex.Location = new System.Drawing.Point(247, 258);
-            this.lblSex.Name = "lblSex";
-            this.lblSex.Size = new System.Drawing.Size(17, 12);
-            this.lblSex.TabIndex = 109;
-            this.lblSex.Text = "11";
-            // 
-            // lblName
-            // 
-            this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(247, 189);
-            this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(17, 12);
-            this.lblName.TabIndex = 108;
-            this.lblName.Text = "11";
-            // 
-            // lblLoginId
-            // 
-            this.lblLoginId.AutoSize = true;
-            this.lblLoginId.Location = new System.Drawing.Point(247, 121);
-            this.lblLoginId.Name = "lblLoginId";
-            this.lblLoginId.Size = new System.Drawing.Size(17, 12);
-            this.lblLoginId.TabIndex = 107;
-            this.lblLoginId.Text = "11";
-            // 
-            // lblCardID
-            // 
-            this.lblCardID.AutoSize = true;
-            this.lblCardID.Location = new System.Drawing.Point(247, 58);
-            this.lblCardID.Name = "lblCardID";
-            this.lblCardID.Size = new System.Drawing.Size(17, 12);
-            this.lblCardID.TabIndex = 103;
-            this.lblCardID.Text = "11";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(101, 338);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(65, 12);
-            this.label6.TabIndex = 100;
-            this.label6.Text = "电话号码：";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(101, 258);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(41, 12);
-            this.label5.TabIndex = 99;
-            this.label5.Text = "性别：";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(101, 189);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(65, 12);
-            this.label4.TabIndex = 98;
-            this.label4.Text = "真实姓名：";
-            // 
-            // lbl1
-            // 
-            this.lbl1.AutoSize = true;
-            this.lbl1.Location = new System.Drawing.Point(101, 58);
-            this.lbl1.Name = "lbl1";
-            this.lbl1.Size = new System.Drawing.Size(77, 12);
-            this.lbl1.TabIndex = 97;
-            this.lbl1.Text = "身份证号码：";
-            // 
-            // lblUser
-            // 
-            this.lblUser.AutoSize = true;
-            this.lblUser.Location = new System.Drawing.Point(101, 121);
-            this.lblUser.Name = "lblUser";
-            this.lblUser.Size = new System.Drawing.Size(53, 12);
-            this.lblUser.TabIndex = 95;
-            this.lblUser.Text = "用户名：";
-            // 
-            // btnCertain
-            // 
-            this.btnCertain.Location = new System.Drawing.Point(146, 420);
-            this.btnCertain.Name = "btnCertain";
-            this.btnCertain.Size = new System.Drawing.Size(75, 23);
-            this.btnCertain.TabIndex = 93;
-            this.btnCertain.Text = "确定";
-            this.btnCertain.UseVisualStyleBackColor = true;
             // 
             // FrmDeleteUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(364, 505);
-            this.Controls.Add(this.lblPhone);
-            this.Controls.Add(this.lblSex);
-            this.Controls.Add(this.lblName);
-            this.Controls.Add(this.lblLoginId);
-            this.Controls.Add(this.lblCardID);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.lbl1);
-            this.Controls.Add(this.lblUser);
-            this.Controls.Add(this.btnCertain);
+            this.ClientSize = new System.Drawing.Size(284, 261);
             this.Name = "FrmDeleteUser";
             this.Text = "删除用户";
-            this.Load += new System.EventHandler(this.FrmDeleteUser_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblPhone;
-        private System.Windows.Forms.Label lblSex;
-        private System.Windows.Forms.Label lblName;
-        private System.Windows.Forms.Label lblLoginId;
-        private System.Windows.Forms.Label lblCardID;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label lbl1;
-        private System.Windows.Forms.Label lblUser;
-        private System.Windows.Forms.Button btnCertain;
     }
 }
